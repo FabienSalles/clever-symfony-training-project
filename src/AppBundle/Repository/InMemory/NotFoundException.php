344efa6dee93f93ea9d4968e7b8a2b396651c62f
FabienSalles <blacked19@gmail.com>
2017-02-14 14:05:35 +0100

    add InMemoryRepository

---
+++ src/AppBundle/Repository/InMemory/NotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fsalles
+ * Date: 14/02/17
+ * Time: 10:46
+ */
+
+namespace AppBundle\Repository\InMemory;
+
+use Exception;
+
+class NotFoundException extends Exception
+{
+
+}
